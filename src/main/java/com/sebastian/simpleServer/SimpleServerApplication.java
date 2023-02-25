9cf047a67b59f39e964ecddc4fab32752b624b0f
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-02-25 19:06:41 -0300

    Primer commit

---
+++ src/main/java/com/sebastian/simpleServer/SimpleServerApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.simpleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServerApplication.class, args);
+	}
+
+}
